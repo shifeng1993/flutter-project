2ea88658f658958dcb5c486f1ffd2a1fc46b9dcd
shifeng <shifeng199307@gmail.com>
2018-11-22 20:42:30 +0800

    添加ios的bundleid，添加左侧抽屉导航中的按钮区域

--- lib/pages/drawerPage/home_page_left_drawer.dart
+++ lib/pages/drawerPage/home_page_left_drawer.dart
@@ -2,14 +2,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:redux/redux.dart';
 
 import '../HomePage/CMDB_home_page.dart';
 import '../HomePage/DevOps_home_page.dart';
 import '../HomePage/ITIL_home_page.dart';
 
 import '../../store/states/AppState.dart';
-import '../../store/actions/index.dart';
 
 class HomePageLeftDrawer extends StatelessWidget {
   final iconSize = 24.0;
@@ -32,122 +30,137 @@ class HomePageLeftDrawer extends StatelessWidget {
       elevation: 0.0, // 去掉阴影
       semanticLabel: '123',
       child: Container(
-        color: const Color(0xff2C3144),
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            _drawerHeader(context, userInfo),
-            ListTile(
-              leading: ImageIcon(
-                AssetImage("assets/icons/left_drawer_cmdb.png"),
-                size: iconSize,
-                color: Color(0xffffffff),
-              ),
-              title: Text(
-                '资产监控',
-                style: TextStyle(
-                  color: const Color(0xffffffff),
-                  fontSize: textSize,
-                ),
-              ),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.pushReplacement(
-                  context,
-                  PageRouteBuilder(
-                    pageBuilder: (context, _, __) {
-                      return CMDBHomePage();
-                    },
-                    transitionDuration: const Duration(milliseconds: 300),
-                    transitionsBuilder: (_, animation, __, child) {
-                      return FadeTransition(
-                        opacity: animation,
-                        child: FadeTransition(
-                          opacity:
-                              Tween(begin: 0.5, end: 1.0).animate(animation),
-                          child: child,
+          color: const Color(0xff2C3144),
+          child: Stack(
+            children: <Widget>[
+              ListView(
+                padding: EdgeInsets.only(top: 20.0),
+                children: <Widget>[
+                  _drawerHeader(context, userInfo),
+                  ListTile(
+                    leading: ImageIcon(
+                      AssetImage("assets/icons/left_drawer_cmdb.png"),
+                      size: iconSize,
+                      color: Color(0xffffffff),
+                    ),
+                    title: Text(
+                      '资产监控',
+                      style: TextStyle(
+                        color: const Color(0xffffffff),
+                        fontSize: textSize,
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.pushReplacement(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder: (context, _, __) {
+                            return CMDBHomePage();
+                          },
+                          transitionDuration: const Duration(milliseconds: 300),
+                          transitionsBuilder: (_, animation, __, child) {
+                            return FadeTransition(
+                              opacity: animation,
+                              child: FadeTransition(
+                                opacity: Tween(begin: 0.5, end: 1.0)
+                                    .animate(animation),
+                                child: child,
+                              ),
+                            );
+                          },
                         ),
                       );
                     },
                   ),
-                );
-              },
-            ),
-            ListTile(
-              leading: ImageIcon(
-                AssetImage("assets/icons/left_drawer_ops.png"),
-                size: iconSize,
-                color: Color(0xffffffff),
-              ),
-              title: Text(
-                '运维中心',
-                style: TextStyle(
-                  color: const Color(0xffffffff),
-                  fontSize: textSize,
-                ),
-              ),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.pushReplacement(
-                  context,
-                  PageRouteBuilder(
-                    pageBuilder: (context, _, __) {
-                      return DevOpsHomePage();
-                    },
-                    transitionDuration: const Duration(milliseconds: 300),
-                    transitionsBuilder: (_, animation, __, child) {
-                      return FadeTransition(
-                        opacity: animation,
-                        child: FadeTransition(
-                          opacity:
-                              Tween(begin: 0.5, end: 1.0).animate(animation),
-                          child: child,
+                  ListTile(
+                    leading: ImageIcon(
+                      AssetImage("assets/icons/left_drawer_ops.png"),
+                      size: iconSize,
+                      color: Color(0xffffffff),
+                    ),
+                    title: Text(
+                      '运维中心',
+                      style: TextStyle(
+                        color: const Color(0xffffffff),
+                        fontSize: textSize,
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.pushReplacement(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder: (context, _, __) {
+                            return DevOpsHomePage();
+                          },
+                          transitionDuration: const Duration(milliseconds: 300),
+                          transitionsBuilder: (_, animation, __, child) {
+                            return FadeTransition(
+                              opacity: animation,
+                              child: FadeTransition(
+                                opacity: Tween(begin: 0.5, end: 1.0)
+                                    .animate(animation),
+                                child: child,
+                              ),
+                            );
+                          },
                         ),
                       );
                     },
                   ),
-                );
-              },
-            ),
-            ListTile(
-              leading: ImageIcon(
-                AssetImage("assets/icons/left_drawer_itil.png"),
-                size: iconSize,
-                color: Color(0xffffffff),
-              ),
-              title: Text(
-                '流程中心',
-                style: TextStyle(
-                  color: const Color(0xffffffff),
-                  fontSize: textSize,
-                ),
-              ),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.pushReplacement(
-                  context,
-                  PageRouteBuilder(
-                    pageBuilder: (context, _, __) {
-                      return ITILHomePage();
-                    },
-                    transitionDuration: const Duration(milliseconds: 300),
-                    transitionsBuilder: (_, animation, __, child) {
-                      return FadeTransition(
-                        opacity: animation,
-                        child: FadeTransition(
-                          opacity:
-                              Tween(begin: 0.5, end: 1.0).animate(animation),
-                          child: child,
+                  ListTile(
+                    leading: ImageIcon(
+                      AssetImage("assets/icons/left_drawer_itil.png"),
+                      size: iconSize,
+                      color: Color(0xffffffff),
+                    ),
+                    title: Text(
+                      '流程中心',
+                      style: TextStyle(
+                        color: const Color(0xffffffff),
+                        fontSize: textSize,
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.pushReplacement(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder: (context, _, __) {
+                            return ITILHomePage();
+                          },
+                          transitionDuration: const Duration(milliseconds: 300),
+                          transitionsBuilder: (_, animation, __, child) {
+                            return FadeTransition(
+                              opacity: animation,
+                              child: FadeTransition(
+                                opacity: Tween(begin: 0.5, end: 1.0)
+                                    .animate(animation),
+                                child: child,
+                              ),
+                            );
+                          },
                         ),
                       );
                     },
+                  )
+                ],
+              ),
+              Positioned(
+                child: new Container(
+                  child: new Text(
+                    "这里是设置或者更换皮肤按钮的预留区域dsadsad",
                   ),
-                );
-              },
-            )
-          ],
-        ),
-      ),
+                  decoration: new BoxDecoration(
+                    color: Colors.red[400],
+                  ),
+                  padding: new EdgeInsets.all(16.0),
+                ),
+                bottom: 0.0,
+              ),
+            ],
+          )),
     );
   }
 
